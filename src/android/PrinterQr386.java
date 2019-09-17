9b7e27c8d29516480140f4743b61f32a0fa1dcbc
youzengjian <youzengjian@gmail.com>
2019-09-18 05:14:19 +0800

    1、新增连接打印机的接口（connectPrinter） 2、getPariedDevices接口删除多余参数

--- src/android/PrinterQr386.java
+++ src/android/PrinterQr386.java
@@ -8,24 +8,30 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import com.qr.print.*;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class PrinterQr386 extends CordovaPlugin {
     private BluetoothAdapter mBtAdapter;
+    private PrintPP_CPCL printPP_cpcl = null;
     
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("getPariedDevices")) {
-            String message = args.getString(0);
-            this.getPariedDevices(message, callbackContext);
+            this.getPariedDevices(callbackContext);
+            return true;
+        } else if (action.equals("connectPrinter")) {
+            String printer_name = args.getString(0);
+            String printer_addr = args.getString(1);
+            this.connectPrinter(printer_name, printer_addr, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void getPariedDevices(String message, CallbackContext callbackContext) {
+    private void getPariedDevices(CallbackContext callbackContext) {
         // Get the local Bluetooth adapter
         this.mBtAdapter = BluetoothAdapter.getDefaultAdapter();
         // Get a set of currently paired devices
@@ -41,6 +47,20 @@ public class PrinterQr386 extends CordovaPlugin {
                 }
             }
         }
-        callbackContext.success(ret.toString());
+        callbackContext.success(ret);
+    }
+
+    private void connectPrinter(String printer_name, String printer_addr, CallbackContext callbackContext) {
+        if (null == this.printPP_cpcl) {
+            this.printPP_cpcl = new PrintPP_CPCL();
+        }
+        JSONObject ret = new JSONObject();
+        try{
+            boolean result = this.printPP_cpcl.connect(printer_name, printer_addr)
+            ret.put("result", result);
+        } catch (JSONException e) {
+            // do nothing
+        }
+        callbackContext.success(ret);
     }
 }
--- www/PrinterQr386.js
+++ www/PrinterQr386.js
@@ -1,5 +1,9 @@
 var exec = require('cordova/exec');
 
-exports.getPariedDevices = function (arg0, success, error) {
-    exec(success, error, 'PrinterQr386', 'getPariedDevices', [arg0]);
+exports.getPariedDevices = function (success, error) {
+    exec(success, error, 'PrinterQr386', 'getPariedDevices', []);
+};
+
+exports.connectPrinter = function (printer_name, printer_addr, success, error) {
+    exec(success, error, 'PrinterQr386', 'connectPrinter', [printer_name, printer_addr]);
 };

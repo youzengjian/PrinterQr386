c7e58dc22fbd5f7b692b4c5787f14a68e71c173a
youzengjian <youzengjian@gmail.com>
2019-09-18 13:27:28 +0800

    1、增加标准接口封装

--- src/android/PrinterQr386.java
+++ src/android/PrinterQr386.java
@@ -22,10 +22,38 @@ public class PrinterQr386 extends CordovaPlugin {
         if (action.equals("getPariedDevices")) {
             this.getPariedDevices(callbackContext);
             return true;
-        } else if (action.equals("connectPrinter")) {
+        } else if (action.equals("connect")) {
             String printer_name = args.getString(0);
             String printer_addr = args.getString(1);
-            this.connectPrinter(printer_name, printer_addr, callbackContext);
+            this.connect(printer_name, printer_addr, callbackContext);
+            return true;
+        } else if (action.equals("disconnect")) {
+            this.disconnect(callbackContext);
+            return true;
+        } else if (action.equals("isConnected")) {
+            this.isConnected(callbackContext);
+            return true;
+        } else if (action.equals("print")) {
+            int horizontal = args.getInt(0);
+            int skip = args.getInt(1);
+            this.print(horizontal, skip, callbackContext);
+            return true;
+        } else if (action.equals("pageSetup")) {
+            int pageWidth = args.getInt(0);
+            int pageHeight = args.getInt(1);
+            this.pageSetup(pageWidth, pageHeight, callbackContext);
+            return true;
+        } else if (action.equals("drawText")) {
+            int text_x = args.getInt(0);
+            int text_y = args.getInt(1);
+            String text = args.getString(2);
+            int fontSize = args.getInt(3);
+            int rotate = args.getInt(4);
+            int bold = args.getInt(5);
+            boolean reverse = args.getBoolean(6);
+            boolean underline = args.getBoolean(7);
+
+            this.drawText(text_x, text_y, text, fontSize, rotate, bold, reverse, underline, callbackContext);
             return true;
         }
         return false;
@@ -50,14 +78,96 @@ public class PrinterQr386 extends CordovaPlugin {
         callbackContext.success(ret);
     }
 
-    private void connectPrinter(String printer_name, String printer_addr, CallbackContext callbackContext) {
+    private void connect(String printer_name, String printer_addr, CallbackContext callbackContext) {
         if (null == this.printPP_cpcl) {
             this.printPP_cpcl = new PrintPP_CPCL();
         }
         JSONObject ret = new JSONObject();
         try{
-            boolean result = this.printPP_cpcl.connect(printer_name, printer_addr)
+            boolean result = this.printPP_cpcl.connect(printer_name, printer_addr);
             ret.put("result", result);
+        } catch (JSONException e) {
+            // do nothing
+        }
+        callbackContext.success(ret);
+    }
+
+    private void disconnect(CallbackContext callbackContext) {
+        JSONObject ret = new JSONObject();
+        try{
+            ret.put("result", true);
+        } catch (JSONException e) {
+            // do nothing
+        }
+        if (null != this.printPP_cpcl) {
+            this.printPP_cpcl.disconnect();
+        }
+        callbackContext.success(ret);
+    }
+
+    private void isConnected(CallbackContext callbackContext) {
+        boolean isConnected = false;
+        if (null != this.printPP_cpcl) {
+            isConnected = this.printPP_cpcl.isConnected();
+        }
+
+        JSONObject ret = new JSONObject();
+        try{
+            ret.put("result", isConnected);
+        } catch (JSONException e) {
+            // do nothing
+        }
+        callbackContext.success(ret);
+    }
+
+    private void print(int horizontal, int skip, CallbackContext callbackContext) {
+        boolean result = false;
+        String message = "Unknown Error";
+        if (null == this.printPP_cpcl || !this.printPP_cpcl.isConnected()) {
+            message = "Device Not Connected";
+        } else {
+            message = this.printPP_cpcl.print(horizontal, skip);
+            if (message.equals("Ok")) {
+                result = true;
+            }
+        }
+        JSONObject ret = new JSONObject();
+        try{
+            ret.put("result", result);
+            ret.put("message", message);
+        } catch (JSONException e) {
+            // do nothing
+        }
+        callbackContext.success(ret);
+    }
+
+    private void pageSetup(int pageWidth, int pageHeight,CallbackContext callbackContext) {
+        JSONObject ret = new JSONObject();
+        try{
+            ret.put("result", true);
+        } catch (JSONException e) {
+            // do nothing
+        }
+        if (null != this.printPP_cpcl) {
+            this.printPP_cpcl.pageSetup(pageWidth, pageHeight);
+        }
+        callbackContext.success(ret);
+    }
+
+    private void drawText(int text_x, int text_y, String text, int fontSize, int rotate, int bold, boolean reverse, boolean underline, CallbackContext callbackContext) {
+        boolean result = false;
+        String message = "Unknown Error";
+        if (null == this.printPP_cpcl || !this.printPP_cpcl.isConnected()) {
+            message = "Device Not Connected";
+        } else {
+            this.printPP_cpcl.drawText(text_x, text_y, text, fontSize, rotate, bold, reverse, underline);
+            result = true;
+            message = "Success";
+        }
+        JSONObject ret = new JSONObject();
+        try{
+            ret.put("result", result);
+            ret.put("message", message);
         } catch (JSONException e) {
             // do nothing
         }
--- www/PrinterQr386.js
+++ www/PrinterQr386.js
@@ -4,6 +4,26 @@ exports.getPariedDevices = function (success, error) {
     exec(success, error, 'PrinterQr386', 'getPariedDevices', []);
 };
 
-exports.connectPrinter = function (printer_name, printer_addr, success, error) {
-    exec(success, error, 'PrinterQr386', 'connectPrinter', [printer_name, printer_addr]);
+exports.connect = function (printer_name, printer_addr, success, error) {
+    exec(success, error, 'PrinterQr386', 'connect', [printer_name, printer_addr]);
 };
+
+exports.disconnect = function (success, error) {
+    exec(success, error, 'PrinterQr386', 'disconnect', []);
+};
+
+exports.isConnected = function (success, error) {
+    exec(success, error, 'PrinterQr386', 'isConnected', []);
+};
+
+exports.print = function (horizontal, skip, success, error) {
+    exec(success, error, 'PrinterQr386', 'print', [horizontal, skip]);
+};
+
+exports.pageSetup = function (pageWidth, pageHeight, success, error) {
+    exec(success, error, 'PrinterQr386', 'pageSetup', [pageWidth, pageHeight])
+}
+
+exports.drawText = function (text_x, text_y, text, fontSize, rotate, bold, reverse, underline, success, error) {
+    exec(success, error, 'PrinterQr386', 'pageSetup', [text_x, text_y, text, fontSize, rotate, bold, reverse, underline])
+}
